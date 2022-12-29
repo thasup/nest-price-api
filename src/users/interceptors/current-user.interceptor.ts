258fbbb5aadaa3614dd2d96e3ab7d793e705bd3c
thasup <thanachonfirst@hotmail.com>
2022-12-29 23:15:25 +0700

    Communicating from interceptor to decorator

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  NestInterceptor,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private userService: UsersService) {}
+
+  async intercept(context: ExecutionContext, handler: CallHandler<any>) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.userService.findOne(userId);
+      request.CurrentUser = user;
+    }
+
+    return handler.handle();
+  }
+}
