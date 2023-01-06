0eff2e4e3f117f6644f05d5bd98c0e195d82f130
thasup <thanachonfirst@hotmail.com>
2023-01-07 01:37:09 +0700

    Apply a global scoped middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +10,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -20,6 +24,25 @@ import { Report } from './reports/reports.entity';
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        // Striped out any extra properties from incoming request body automatically
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['someString'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
