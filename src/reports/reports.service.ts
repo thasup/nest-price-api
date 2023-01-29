a8caaca0b2c9aec22e6b2b8690b66bde928d0a85
thasup <thanachonfirst@hotmail.com>
2023-01-29 23:10:44 +0700

    Testing the estimate logic

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -10,22 +10,16 @@ import { Report } from './reports.entity';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) { }
 
-  async createEstimate({
-    make,
-    model,
-    lng,
-    lat,
-    year,
-    mileage,
-  }: GetEstimateDto) {
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
     return this.repo
       .createQueryBuilder()
       .select('AVG(price', 'price')
-      .where('make = :make', { make })
-      .andWhere('model = :model', { model })
+      .where('LOWER(make) = LOWER(:make)', { make })
+      .andWhere('LOWER(model) = LOWER(:model)', { model })
       .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
       .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
       .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
       .orderBy('ABS(mileage - :mileage)', 'DESC')
       .setParameters({ mileage })
       .limit(3)
